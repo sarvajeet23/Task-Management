0ee9162014c84d709e9d2b775355963cdd66db40
sarvajeet23 <Sarvajeet.office@gmail.com>
2024-06-07 10:35:21 +0530

    modified

--- lib/constant/app_string.dart
+++ lib/constant/app_string.dart
@@ -1,4 +1,5 @@
 abstract class AppString {
   // static const baseUrl = "http://localhost:8000";
-  static const baseUrl = "http://10.0.2.2:8000";
+  // static const baseUrl = "http://10.0.2.2:8000";
+  static const baseUrl = "http://127.0.0.1:8000";
 }
